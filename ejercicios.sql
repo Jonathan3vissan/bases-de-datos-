422375e453450c8902179022f9367d864ffec519
-1 <>
2024-06-11 11:25:56 -0300

    incinado ejercicios de sqlbotl

---
+++ ejercicios.sql
@@ -0,0 +1,5 @@
+
+/* con el primer comando distinct evita duplicados, con order by ordena los resultado y asc hace que eete orden sea alfabeticamente*/ 
+SELECT distinct director FROM movies  order by director asc ;
+
+
